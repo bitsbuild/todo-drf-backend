d095b899f0543ad0b09d265c7c13af6b6572a68a
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-10 19:49:10 +0530

    Pending Set-Up Imports

--- todo/todorestapi/views.py
+++ todo/todorestapi/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 def sample(request):
     return HttpResponse("Sample Works Well")
