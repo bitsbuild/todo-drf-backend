b6484c8306de384689a5fb0fc3693bd246918316
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-10 22:14:21 +0530

    Find Task By ID Completed

--- todo/todorestapi/urls.py
+++ todo/todorestapi/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from todorestapi.views import add_an_item,get_all_to_do_items,get_specific_to_do_item,remove_an_item,update_an_item
 urlpatterns = [
     path('api/tasks/', get_all_to_do_items),
-    path('api/tasks/find/', get_specific_to_do_item),
+    path('api/tasks/find/<uuid:id>', get_specific_to_do_item),
     path('api/tasks/add/', add_an_item),
     path('api/tasks/update/', update_an_item),
     path('api/tasks/delete/', remove_an_item),
--- todo/todorestapi/views.py
+++ todo/todorestapi/views.py
@@ -4,20 +4,37 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from todorestapi.models import Task
 from todorestapi.serializers import TaskSerializer
+
+
+
 @api_view(['GET'])
 def get_all_to_do_items(request):
     tasks = Task.objects.all()
     serialized_tasks = TaskSerializer(tasks,many=True)
     return Response(serialized_tasks.data)
 @api_view(['GET'])
-def get_specific_to_do_item(request):
-    return HttpResponse("")
+
+
+
+def get_specific_to_do_item(request,id):
+    required_task = Task.objects.get(pk=id)
+    serialized_required_task = TaskSerializer(required_task)
+    return Response(serialized_required_task.data)
+
+
+
 @api_view(['POST'])
 def add_an_item(request):
     return HttpResponse("")
+
+
+
 @api_view(['PUT'])
 def update_an_item(request):
     return HttpResponse("")
+
+
+
 @api_view(['DELETE'])
 def remove_an_item(request):
     return HttpResponse("")
